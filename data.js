d60e307ad5c8afe336b6f3b0d9a2b10608831bfa
Aman5axena <164467902+Aman5axena@users.noreply.github.com>
2024-03-22 13:30:49 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'The mercurial villian Loki resumes his role as the God of Mischeif in a new series that takes place after the events of "Avengers:  Endgame.',
+        image: 'images/25.PNG'
+    },
+    {
+        name: 'M.S.Dhoni: The Untold Story',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/24.PNG'
+    },
+    {
+        name: 'Salaar',
+        des: `In the crime-infested Khansaar, Prince Varadha sets to ascend the throne. But a d'etat is planned! And there's only one man to help reclaim power: Deva`,
+        image: 'images/26.PNG'
+    },
+    {
+        name: `Ashwin Reigns Supreme On Home Soil`,
+        des: 'Check out the best stats from Day 3 of the fourth Test between India and England',
+        image: 'images/2.PNG'
+    }
+]
